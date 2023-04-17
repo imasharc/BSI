038744587860e6d04643d80f2eea8e4b2a569768
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-04-17 09:51:03 +0200

    Update notes for module 3 ([CRYPTOGRAPHY] Hash functions) from BSI, add python script to indentify hash function used

---
+++ materials_and_solutions/cryptography_modules/hash_functions/indentify_hash_func.py
@@ -0,0 +1,17 @@
+﻿import hashlib
+import sys
+
+if (len(sys.argv) != 3):
+    raise Exception('Syntax: python3 program.py string_in hash_out')
+
+string_in = sys.argv[1].encode('utf-8')
+hash_out = sys.argv[2]
+
+for alg in hashlib.algorithms_available:
+    try:
+        hash_tmp = hashlib.new(alg)
+        hash_tmp.update(string_in)
+        if hash_tmp.hexdigest() == hash_out:
+            print('Algorithm used: ', alg)
+    except:
+        print('Exception: ', alg)
