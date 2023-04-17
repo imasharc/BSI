2567d7c18714174ab5d2ceb7a65f4c6ff0f0a3ed
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-04-17 12:28:37 +0200

    Extract python script to a new file, script takes string as an input parameter and prints out the encoded sha3 string

---
+++ materials_and_solutions/cryptography_modules/hash_functions/sha3_encode.py
@@ -0,0 +1,9 @@
+﻿import hashlib
+import sys
+
+if (len(sys.argv) != 2):
+    raise Exception('Syntax: python3 program.py string_in')
+
+sha3 = hashlib.sha3_512()
+sha3.update(sys.argv[1].encode('utf-8'))
+print(sha3.digest())
